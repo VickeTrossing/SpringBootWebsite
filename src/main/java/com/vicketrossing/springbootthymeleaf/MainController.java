2817e53fecc9d066f59a5b74ee2927fa321a7513
VickeTrossing <vitro001@gmail.com>
2021-09-19 00:39:54 +0200

    Just added a login page, its not working yet relax

--- src/main/java/com/vicketrossing/springbootthymeleaf/MainController.java
+++ src/main/java/com/vicketrossing/springbootthymeleaf/MainController.java
@@ -15,4 +15,9 @@ public class MainController {
     public String showUsers(){
         return "users";
     }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
 }
